f0d54f299ec4ded5534199d543bc76df5eaecdca
Carlos Salazar <crslarez@gmail.com>
2022-11-08 16:47:21 -0500

    cambia a por form

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#form");
+let uno= document.querySelector("#uno")
+
+button.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    console.log(123);
+})
