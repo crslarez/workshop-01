668e6c7f18faa04e8b8c540024f53713fddb997e
Carlos Salazar <crslarez@gmail.com>
2022-11-09 17:23:57 -0500

    change span for input

--- main.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("#form");
-let uno= document.querySelector("#uno")
-
-button.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    console.log(123);
-})
